3cc461976498e6d944e3c05cba60eb683f36cd3d
Dominik Toušek <tousekdominik@gmail.com>
2018-01-29 19:34:33 +0100

    R1.5.1 * updated /pdbid script - Pores init

--- static/pdbid/index.php
+++ static/pdbid/index.php
@@ -2,7 +2,7 @@
 //https://api.mole.upol.cz/Init/<pdbid>
 
 $pdbid = $_GET["pdbid"];
-$resultJson = file_get_contents("http://api.mole.upol.cz/Init/".$pdbid);
+$resultJson = file_get_contents("http://api.mole.upol.cz/Init/Pores/".$pdbid);
 $json = json_decode($resultJson);
 
 header('Location: \online/'.$json->ComputationId);
