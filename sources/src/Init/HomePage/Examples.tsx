c1dcd00f89b04b764326be637d1f2423f2d0bc2c
Dušan Veľký <dvelky@mail.muni.cz>
2024-12-19 00:26:47 +0100

    Update examples

--- sources/src/Init/HomePage/Examples.tsx
+++ sources/src/Init/HomePage/Examples.tsx
@@ -20,7 +20,7 @@ export class Examples extends React.Component<{}, { activeButton: number }> {
     componentDidMount() {
     }
 
-    handleButtonClick(index: number) {
+    handleImageClick(index: number) {
         this.setState({ activeButton: index })
     }
 
@@ -121,100 +121,79 @@ export class Examples extends React.Component<{}, { activeButton: number }> {
     }
 
     examples = [
-        { id: '5VKQ', src: '/images/example_5VKQ.png', title: 'Mechanotransduction channel NOMPC', doi: '10.1038/nature22981' },
-        { id: '1TQN', src: '/images/example_1TQN.png', title: 'Solvent channel of cytochrome P450 3A4', doi: '10.1074/jbc.C400293200' },
-        { id: '1EVE', src: '/images/example_1EVE.png', title: 'Channels of acetylcholinesterase', doi: '10368299' },
-        { id: '4CZH', src: '/images/example_4CZH.png', title: 'Channels of haloalkane dehalogenase', doi: '10.1016/j.febslet.2014.02.056' },
-        { id: '1K4C', src: '/images/example_1K4C.png', title: 'Potassium Channel KcsA-Fab', doi: '10.1038/35102009' },
+        { id: '1GPN', src: '/images/example_1GPN.png', title: '1GPN - Structure of acetylcholinesterase complexed with huperzine B' },
+        { id: '1V9E', src: '/images/example_1V9E.png', title: '1V9E - Crystal Structure Analysis of Bovine Carbonic Anhydrase II' },
+        { id: '2V61', src: '/images/example_2V61.png', title: '2V61 - Structure of human MAO B in complex with the selective inhibitor 7-(3- chlorobenzyloxy)-4-(methylamino)methyl-coumarin' },
+        { id: '2VV5', src: '/images/example_2VV5.png', title: '2VV5 - The open structure of MscS' },
+        { id: '2XN8', src: '/images/example_2XN8.png', title: '2XN8 - X-ray structure of the substrate-free Mycobacterium tuberculosis cytochrome P450 CYP125' },
+        { id: '3LDC', src: '/images/example_3LDC.png', title: '3LDC - Open MthK pore structure crystallized in 100 mM K+' },
     ];
 
     render() {
-        console.log(this.state.activeButton);
         return (
             <div className="container-fluid text-center" style={{ marginBottom: '10rem' }}>
                 <div className="row">
                     <div className="col">
-                        <h3>Examples</h3>
+                        <h4><b>Examples</b></h4>
                     </div>
                 </div>
                 <div className="row mb-3">
                     {this.examples.slice(0, 3).map((example, index) => (
-                        <div key={example.id} className="col d-flex flex-column justify-content-center align-items-center">
-                            <figure className="figure" style={{ maxWidth: '28rem' }}>
+                        <div key={example.id} className="col d-flex flex-column justify-content-center align-items-center position-relative">
+                            {this.state.activeButton === index && (
+                                <div className="spinner-overlay">
+                                    <div className="spinner-border text-primary" role="status">
+                                        <span className="visually-hidden">Loading...</span>
+                                    </div>
+                                </div>
+                            )}
+                            <figure
+                                className={`figure ${this.state.activeButton !== -1 && this.state.activeButton !== index ? 'disabled' : ''}`}
+                                style={{ maxWidth: '28rem', cursor: 'pointer' }}
+                                onClick={() => { if (this.examples[index]) { this.handleImageClick(index); this.handleFormSubmit(this.examples[index].id); } }}
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                title={example.title} // Tooltip content
+                            >
                                 <img
                                     className="img-fluid figure-img"
                                     style={{ objectFit: 'contain' }}
                                     src={example.src}
-                                    alt={`${example.title} (PDB ID: ${example.id})`}
+                                    alt={example.title}
                                 />
-                                <figcaption>
-                                    {example.title} (PDB ID:{' '}
-                                        {example.id}
-                                    ); DOI:{' '}
-                                    <a target="_blank" href={`https://doi.org/${example.doi}`}>
-                                        {example.doi}
-                                    </a>
-                                </figcaption>
                             </figure>
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                disabled={this.state.activeButton !== -1 && this.state.activeButton !== index}
-                                onClick={() => {if (this.examples[index]) {this.handleButtonClick(index); this.handleFormSubmit(this.examples[index].id)}}}
-                                title="Open in MOLEonline"
-                            >
-                                {this.state.activeButton === index ? (
-                                    <div className="d-flex align-items-center">
-                                        <span className="spinner-border spinner-border-sm align-middle" aria-hidden="true"></span>
-                                        <span role="status">Loading...</span>
-                                    </div>
-                                ) : (
-                                    example.id
-                                )}
-                            </button>
                         </div>
                     ))}
                 </div>
                 <div className="row mb-3">
                     {this.examples.slice(3).map((example, index) => (
-                        <div key={example.id} className="col d-flex flex-column justify-content-center align-items-center">
-                            <figure className="figure" style={{ maxWidth: '28rem' }}>
+                        <div key={example.id} className="col d-flex flex-column justify-content-center align-items-center position-relative">
+                            {this.state.activeButton === index + 3 && (
+                                <div className="spinner-overlay">
+                                    <div className="spinner-border text-primary" role="status">
+                                        <span className="visually-hidden">Loading...</span>
+                                    </div>
+                                </div>
+                            )}
+                            <figure
+                                className={`figure ${this.state.activeButton !== -1 && this.state.activeButton !== index + 3 ? 'disabled' : ''}`}
+                                style={{ maxWidth: '28rem', cursor: 'pointer' }}
+                                onClick={() => { if (this.examples[index + 3]) { this.handleImageClick(index + 3); this.handleFormSubmit(this.examples[index + 3].id); } }}
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                title={example.title} // Tooltip content
+                            >
                                 <img
                                     className="img-fluid figure-img"
                                     style={{ objectFit: 'contain' }}
                                     src={example.src}
-                                    alt={`${example.title} (PDB ID: ${example.id})`}
+                                    alt={example.title}
                                 />
-                                <figcaption>
-                                    {example.title} (PDB ID:{' '}
-                                        {example.id}
-                                    ); DOI:{' '}
-                                    <a target="_blank" href={`https://doi.org/${example.doi}`}>
-                                        {example.doi}
-                                    </a>
-                                </figcaption>
                             </figure>
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                disabled={this.state.activeButton !== -1 && this.state.activeButton !== index + 3}
-                                onClick={() => {if (this.examples[index + 3]) {this.handleButtonClick(index + 3); this.handleFormSubmit(this.examples[index + 3].id)}}}
-                                title="Open in MOLEonline"
-                            >
-                                {this.state.activeButton === index + 3 ? (
-                                    <div className="d-flex align-items-center">
-                                        <span className="spinner-border spinner-border-sm align-middle" aria-hidden="true"></span>
-                                        <span role="status">Loading...</span>
-                                    </div>
-                                ) : (
-                                    example.id
-                                )}
-                            </button>
                         </div>
                     ))}
                 </div>
             </div>
-        )
+        );
     }
 }
-
